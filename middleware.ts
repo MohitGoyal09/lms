a692244dc1703d55fef6761aae4ea156da03c906
MohitGoyal09 <goel0632@gmail.com>
2025-01-06 16:17:29 +0530

    Enhance middleware: skip authentication for root URL and refine route matcher configuration

--- middleware.ts
+++ middleware.ts
@@ -9,13 +9,14 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
+  if (req.url.endsWith("/")) return;
+
   if (isProtectedRoute(req)) await auth.protect();
 });
 
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-
     "/(api|trpc)(.*)",
   ],
 };
